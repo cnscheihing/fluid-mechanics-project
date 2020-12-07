1a8325cebac5a099b53569155143f1666e1a3804
cnscheihing <cnscheihing@uc.cl>
2020-12-06 23:52:35 -0300

    ✨ final plot and refactor

--- app/main.py
+++ app/main.py
@@ -19,7 +19,9 @@ app.config['SECRET_KEY'] = SECRET_KEY
 
 class CustomForm(FlaskForm):
     drag_coef = DecimalField(
-        'Valor de coeficiente de arrastre', validators=[DataRequired()])
+        'Valor de coeficiente de arrastre (Cd)', validators=[DataRequired()])
+    sust_coef = DecimalField(
+        'Valor de coeficiente de sustentación (Cl)', validators=[DataRequired()])
     submit = SubmitField('Cargar')
 
 
@@ -27,49 +29,42 @@ class CustomForm(FlaskForm):
 def home():
     form = CustomForm()
     if form.validate_on_submit():
-        return render_template('index.html', form=form, coef=form.drag_coef.data)
-    return render_template('index.html', form=form, coef=0)
+        return render_template('index.html', form=form, drag_coef=form.drag_coef.data, sust_coef=form.sust_coef.data)
+    return render_template('index.html', form=form, drag_coef=0, sust_coef=0)
 
 
-@app.route('/plot-<drag_coef>.png')
-def plot_png(drag_coef):
-    fig = create_figure(float(drag_coef))
+@app.route('/plot/<drag_coef>/<sust_coef>.png')
+def plot_png(drag_coef, sust_coef):
+    fig = create_figure(float(drag_coef), float(sust_coef))
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
-# AQUI RELLENAR CON FUNCION QUE GENERA GRAFICO
 
-
-def create_figure(coef):
-    ca = coef
+def create_figure(drag_coef, sust_coef):
+    ca = drag_coef
     rho = 1.2
     A = 0.1
-    velocidades = np.linspace(0, 35*3.6, 36*3.6)
+    velocidades = np.linspace(0, 3.6*35, 3.6*36)
     cte = 0.5*rho*A*ca
+    cs = sust_coef
+    cte_sus = 0.5*rho*cs
+
+    sustentacion = []
     arrastre = []
 
     for vel in velocidades:
         arrastre.append(vel*vel*cte)
-
-    # plt.xlabel("Velocidad (m/s)")
-    # plt.ylabel("Fuerza de Arrastre (N)")
-    # plt.title("Fuerza de Arrastre vs Velocidad")
+        sustentacion.append(vel*vel*cte_sus)
     fig = Figure()
-    # plt
     axis = fig.add_subplot(1, 1, 1)
-    # xs = range(100)
-    # ys = [random.randint(1, 50) * coef for x in xs]
-    axis.set_ylim([0, 1500])
-    axis.plot(velocidades, arrastre)
-    # plt.plot(velocidades, arrastre)
-    # plt.xlabel('xlabel', fontsize=18)
-    # plt.ylabel('ylabel', fontsize=16)
-    # plt.savefig('/static/img/tablas/test.png')
+    axis.plot(velocidades, arrastre, color="blue", label="Fuerza de Arrastre")
+    axis.plot(velocidades, sustentacion, color="red",
+              label="Fuerza de Sustenación")
     fig.text(0.5, 0.04, 'Velocidad (km/h)', ha='center')
-    fig.text(0, 0.5, 'Fuerza de Arrastre (N)',
-             va='center', rotation='vertical')
-    axis.title.set_text('Fuerza de Arrastre vs Velocidad')
+    fig.text(0, 0.5, 'Fuerza (N)', va='center', rotation='vertical')
+    axis.title.set_text('Fuerza de Arrastre y Sustentación vs Velocidad')
+    axis.legend(loc='upper left', frameon=True)
     return fig
 
 
